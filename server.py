13caafdb00c2957f99e0f9bda3d979ad81727913
msu-web-dev <sundhei4@msu.edu>
2014-01-14 15:47:09 -0500

    Update server.py
    Used http://stackoverflow.com/questions/8315209/sending-http-headers-with-python

--- server.py
+++ server.py
@@ -18,6 +18,8 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
+    c.send('HTTP/1.0 200 OK\r\n')
+    c.send('<html><body><h1>Hello World</body></html>')
     c.send('Thank you for connecting')
     c.send("good bye.")
     c.close()
