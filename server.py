7e902afcf2d41dd0b11cdd4f1d6c3ef6941da8d3
Andrew Sundheimer <sundhei4@msu.edu>
2014-01-15 20:15:26 -0500

    Committing a working copy of server.py with Hello World. Also removed comments

--- server.py
+++ server.py
@@ -3,24 +3,21 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
+s = socket.socket()
+host = socket.getfqdn()
 port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+s.bind((host, port))
+
+helloWorld = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n<html><body><h1>Hello World</h1>This is msu-web-dev's Web Server.</body></html>"
 
 print 'Starting server on', host, port
 print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
-
+s.listen(5)
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
-    # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('HTTP/1.0 200 OK\r\n')
-    c.send('Context-type: text/html')
-    c.send('<html><body><h1>Hello World</h1></body></html>')
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send(helloWorld)
     c.close()
