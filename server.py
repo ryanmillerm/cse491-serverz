17d44396229ff84ba25bced3a021d25c42dbb448
Ryan Miller <mill1256@arctic.cse.msu.edu>
2014-01-16 16:12:15 -0500

    review of hw1

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ port = random.randint(8000, 9999)
 s.bind((host, port))
 
 helloWorld = "HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n<html><body><h1>Hello World</h1>This is msu-web-dev's Web Server.</body></html>"
-
+# @comment Don't forget to format to a max width of 80 chars for readability
 print 'Starting server on', host, port
 print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
